e4a381e2dc382237681866ff4275a1c968a224a5
hempalS39 <hs527196@gmail.com>
2023-04-20 20:27:59 +0530

    assignment-get-api

--- src/routes/route.js
+++ src/routes/route.js
@@ -8,45 +8,82 @@ router.get('/test-me', function (req, res) {
     res.send('This should be working!')
 });
 
-router.get('/test-you', function (req, res) {
-    console.log('The exported module is: ',commonFile)
-    commonFile.doSomething()
-    console.log('This is the constant I created', commonFile.name)
-    res.send('Hello there, welcome to this application!')
+
+let moviesArr = ["Rang de basantiz", "The shining", "Lord of the rings", "Batman begins"] 
+
+// --------- problem -1
+router.get('/movies', function (req, res) {
+
+    res.send(moviesArr)
 });
 
-router.get('/test-underscore', function(req, res){
-    let result = myUnderscore.first([11,12,23,44,15], 4)
-    console.log('the result is',result)
-    res.send('done')
+//----------problem 2 & 3
+router.get('/movies/:index', function(req, res){
+
+    let requiredIdx = req.params.index
+    
+    if(requiredIdx>=0 && requiredIdx<moviesArr.length) {
+        let requiredMovie = moviesArr[requiredIdx]
+        res.send(requiredMovie)
+    }
+    else {
+        res.send("pls provide valid index ")
+    }
+
 })
 
-router.get('/cohorts', function (request, response){
-    // logic to get the cohorts from database
-    // logic tp get only the active cohorts
-    // logic to get only the cohort with a size than 50
-    // logic to get only the backend cohorts
-    response.send(['technetium','nobelium'])
+
+// -----------  
+let movieObj = [ {
+    id: 1,
+    name: "The Shining"
+   }, {
+    id: 2,
+    name: "Incendies"
+   }, {
+    id: 3,
+    name: "Rang de Basanti"
+   }, {
+    id: 4,
+    name: "Finding Nemo"
+   }]
+   
+
+// ----------- problem 4    
+router.get('/films', function (request, response){
+    
+    response.send(movieObj)
 })
 
-router.get('/students', function(req, res){
-    // receive or access the query params in the code
-    // write a logic on these query params
-    // city, score
-    console.log(req.query)
-    let requestedCity = req.query.city
-    let sortField = req.query.sort
-    // logic to get students
-    res.send(["Sabiha","Neha","Akash","Sonali"])
+
+// ------------ problem 5
+router.get('/films/:filmId', function(req, res) {
+   let id = req.params.filmId
+   if(id>=0 && id<movieObj.length) {
+        for(let i=0; i<movieObj.length; i++){
+            if(movieObj[i].id == id){
+                res.send(movieObj[i].name)
+            }
+        }
+   }
+    else {
+        res.send("No movie exists with this id")
+    }
 })
 
-router.get('/students/:studentName', function(req, res) {
-    console.log(req.params.studentName)
-    /// go to database and search for studentName student
-    // store the data found in this variable - studentDetails
-    //res.send({data: studentDetails})
-    res.send('student data')
-})
+
+// router.get('/films', function(req, res){
+//     // receive or access the query params in the code
+//     // write a logic on these query params
+//     // city, score
+//     console.log(req.query)
+//     let requestedCity = req.query.city
+//     let sortField = req.query.sort
+//     // logic to get students
+//     res.send(["Sabiha","Neha","Akash","Sonali"])
+// })
+
+
 
 
 module.exports = router;
