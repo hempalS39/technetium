c5c42c288d129d223c6a791682f2c05831cd9c07
hempalS39 <hs527196@gmail.com>
2023-04-25 19:47:20 +0530

    post-api-assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,67 +7,81 @@ router.get('/students/:name', function(req, res) {
     res.send(studentName)
 })
 
-router.get("/random" , function(req, res) {
-    res.send("hi there")
+//------------------------------ missing number assignment
+router.get('/sol1' , function (req,res) {
+    let arr = [1,2,3,4,5,7];
+    let n =arr[arr.length-1]
+    let totalSum = n*(n+1)/2
+    let sum = 0;
+    for(let i=0; i<arr.length; i++){
+        sum += arr[i]
+    }
+    
+    let missingNum = totalSum-sum;
+
+    return res.send({ missingNum})
 })
 
-
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
-
-
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
-
-
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
-
-
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-
-
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
-
-
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
-
-    // console.log( id , pwd)
-
-    console.log( req.body )
-
-    res.send(  { msg: "hi" , status: true }  )
+router.get('/sol2' , function (req,res) {
+    let arr = [33,34,36,37,38];
+    // let arr = [21,22,23,24,26,27,28,29]
+    let min = arr[0];
+    for(let i=1; i<arr.length; i++){
+        if(arr[i] == min+1) {
+            min = min+1;
+        }
+        else return res.send({misingNum : min+1})
+    }
 })
 
 
 
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
-})
+
+
+//-------------------------------------- post Assignment --------------------------------
+let players =[
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ]
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ]
+       },
+   ]
+
+router.post('/players' , function (req,res) {
+    let player = req.body;
+    
+    for(let i=0; i<players.length; i++){
+        if(players[i].name == player.name){
+           return res.send({msg : "player with same name already present"})
+        }
+    }
+
+    players.push(player)
+   return res.send({data : players})
+})   
+
+
 
 module.exports = router;
